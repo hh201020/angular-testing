05fa076d44f6a998fc3bbbb14e42b8d7b76049b0
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-10-09 12:11:28 -0400

    another similar way to export excel by Sheetjs

--- src/app/excel/excel.component.ts
+++ src/app/excel/excel.component.ts
@@ -1,47 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { utils, write, WorkBook } from 'xlsx';
+/* xlsx.js (C) 2013-present SheetJS -- http://sheetjs.com */
+/* vim: set ts=2: */
+import { Component } from '@angular/core';
+
+import * as XLSX from 'xlsx';
+
 import { saveAs } from 'file-saver';
 
+type AOA = Array<Array<any>>;
+
+function s2ab(s: string): ArrayBuffer {
+	const buf: ArrayBuffer = new ArrayBuffer(s.length);
+	const view: Uint8Array = new Uint8Array(buf);
+	for (let i = 0; i !== s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+	return buf;
+}
+
 @Component({
+	selector: 'sheetjs',
   templateUrl: './excel.component.html',
-  styleUrls: ['./excel.component.scss'],
+  styleUrls: ['./excel.component.scss']
 })
-export class ExcelComponent implements OnInit {
-  title = 'app';
-  table = [
-    {
-      First: 'one',
-      Second: 'ttwo',
-      Third: 'three',
-      Forth: 'ffour',
-      Fifth: 'five'
-    },
-    {
-      First: 'uun',
-      Second: 'ddeux',
-      Third: 'ttrois',
-      Forth: 'qquatre',
-      Fifth: 'ccinq'
-    },
-  ];
 
-  ngOnInit() {
-    const ws_name = 'SomeSheet';
-    const wb: WorkBook = { SheetNames: [], Sheets: {} };
-    const ws: any = utils.json_to_sheet(this.table);
-    wb.SheetNames.push(ws_name);
-    wb.Sheets[ws_name] = ws;
-    const wbout = write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+export class ExcelComponent {
+	data: AOA = [ [1, 2], [3, 4] ];
+	wopts: XLSX.WritingOptions = { bookType: 'xlsx', bookSST: true, type: 'binary' };
+	fileName: string = 'SheetJS.xlsx';
 
-    function s2ab(s) {
-      const buf = new ArrayBuffer(s.length);
-      const view = new Uint8Array(buf);
-      for (let i = 0; i !== s.length; ++i) {
-        view[i] = s.charCodeAt(i) & 0xFF;
-      };
-      return buf;
-    }
+	onFileChange(evt: any) {
+		/* wire up file reader */
+		const target: DataTransfer = <DataTransfer>(evt.target);
+		if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+		const reader: FileReader = new FileReader();
+		reader.onload = (e: any) => {
+			/* read workbook */
+			const bstr: string = e.target.result;
+			const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
 
-    saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }), 'exported.xlsx');
-  }
+			/* grab first sheet */
+			const wsname: string = wb.SheetNames[0];
+			const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+			/* save data */
+			this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+		};
+		reader.readAsBinaryString(target.files[0]);
+	}
+
+	export(): void {
+		/* generate worksheet */
+		const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+		/* generate workbook and add the worksheet */
+		const wb: XLSX.WorkBook = XLSX.utils.book_new();
+		XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+		/* save to file */
+		const wbout: string = XLSX.write(wb, this.wopts);
+		saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }), this.fileName);
+	}
 }
--- src/app/excel/excel.module.ts
+++ src/app/excel/excel.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 // containers
 import { ExcelComponent } from './excel.component';
@@ -11,6 +12,7 @@ export const ROUTES: Routes = [
 
 @NgModule({
   imports: [
+    CommonModule,
     RouterModule.forChild(ROUTES)
   ],
   declarations: [
